0c6f10368bd216c80df3843eb92b609039e10520
pinkimy <antonp601@gmail.com>
2026-01-29 22:30:34 +0200

    Enhance UI for login and registration pages with new layout and styling

--- login.php
+++ login.php
@@ -1,12 +1,48 @@
-<!-- register form-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login or Registration</title>
+</head>
 <body>
     <div>
-        <form action="code.php" method="post">
-            <label >email:</label><br>
-            <input type="text" name="email"><br>
-            <label >password:</label><br>
-            <input type="password" name="password"><br>
-            <input type="submit" value="Log in">
-        </form>
+        <div class="background">
+        <div class="container">
+            <div class="screen">
+                <div class="screen-header">
+                    <div class="screen-header-left">
+                        <div class="screen-header-button close"></div>
+                            <div class="screen-header-button maximize"></div>
+                            <div class="screen-header-button minimize"></div>
+                        </div>
+                            <div class="screen-header-right">
+                            <div class="screen-header-ellipsis"></div>
+                        <div class="screen-header-ellipsis"></div>
+                    <div class="screen-header-ellipsis"></div>
+                </div>
+            </div>
+                    <div class="screen-body">
+                        <div class="screen-body-item left">
+                        <div class="app-title">
+                            <span>Login</span>
+                        </div>
+                        <div class="screen-body-item">  
+                            <form action="code.php" method="post">
+                                <div class="app-form">
+                                    <div class="app-form-group">
+                                        <label >email:</label>
+                                        <input class="app-form-control" type="text" name="email">
+                                        <label >password:</label>
+                                        <input class="app-form-control" type="password" name="password">
+                                        <div class="app-form-group buttons">
+                                        <input class="app-form-button" type="submit" value="Log in">
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
     </div>
 </body>
--- register.php
+++ register.php
@@ -2,6 +2,14 @@
     session_start();
 ?>
 <!-- register form-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration</title>
+</head>
 <body>
     <div>
         <div class="alert">
@@ -13,18 +21,49 @@
             ?>
         </div>
     </div>
-    <div>
-        <h5>Registration</h5>
-        <form action="code.php" method="post">
-        <label >name:</label><br>
-        <input type="text" name="name"><br>
-        <label >email:</label><br>
-        <input type="text" name="email"><br>
-        <label >password:</label><br>
-        <input type="password" name="password"><br>
-        <label >confirm password:</label><br>
-        <input type="password" name="comfir_mpassword"><br>
-        <input type="submit" name = "register_btn"value="Registration">
-        </form>
-    </div>
+    <div class="background">
+        <div class="container">
+            <div class="screen">
+                <div class="screen-header">
+                    <div class="screen-header-left">
+                        <div class="screen-header-button close"></div>
+                            <div class="screen-header-button maximize"></div>
+                            <div class="screen-header-button minimize"></div>
+                        </div>
+                            <div class="screen-header-right">
+                            <div class="screen-header-ellipsis"></div>
+                        <div class="screen-header-ellipsis"></div>
+                    <div class="screen-header-ellipsis"></div>
+                </div>
+            </div>
+                    <div class="screen-body">
+                        <div class="screen-body-item left">
+                        <div class="app-title">
+                            <span>Registration</span>
+                        </div>
+                        <div class="screen-body-item">  
+                            <form action="code.php" method="post">
+                                <div class="app-form">
+                                    <div class="app-form-group">
+                                        <label >name:</label>
+                                        <input class="app-form-control" type="text" name="name">
+                                        <label >email:</label>
+                                        <input class="app-form-control" type="text" name="email">
+                                        <label >password:</label>
+                                        <input class="app-form-control" type="password" name="password">
+                                        <label >confirm password:</label>
+                                        <input class="app-form-control" type="password" name="comfir_mpassword">
+                                        <div class="app-form-group buttons">
+                                            <input class="app-form-button" type="submit" name = "register_btn"value="Registration">
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="credits">github.com/pinkimy
+            </div>
 </body>
+</html>
